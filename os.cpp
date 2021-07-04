c491ddf394e000f3f171ba431b70caa192370b18
Saad Abdullah <f190108@cfd.nu.edu.pk>
2021-07-04 21:52:50 +0500

    init commmit

---
+++ os.cpp
@@ -0,0 +1,18 @@
+//Author: Saad Abdullah | Mahnoor Abid | Bilal Haider
+//Operating SYSTEM Program
+#include <iostream>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <ctime>
+using namespace std;
+
+
+int main()
+{
+
+  return 0;
+}
